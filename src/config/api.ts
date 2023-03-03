31d9be0348dde82725dfbac5c22f6d4c4d7bc48d
Rostislav Berezhnoy <berezhnoy.rostislav@gmail.com>
2023-03-03 21:56:25 +0300

    feat: move urls to env

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:7070/api'
-export const FAKE_API_URL = 'http://localhost:5000'
+export const API_URL = process.env.REACT_APP_API_URL || ''
+export const FAKE_API_URL = process.env.REACT_APP_FAKE_API_URL || ''
